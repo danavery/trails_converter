d502b9f4061662da77886f7f7d8f53346515666f
Dan Avery <dan.github@danavery.com>
2013-06-18 16:04:00 -0700

    first working (un)projection

--- shapefile_convert.rb
+++ shapefile_convert.rb
@@ -7,9 +7,12 @@ class ShapefileConverter
     @textfile_field_names.push("GEOM")
   end
 
-  def convert_file() 
-    RGeo::Shapefile::Reader.open(@shpfile, 
-                                 factory: RGeo::Geographic.spherical_factory(srid: @srid)) do |file|
+  def convert_file()
+    puts "srid: #{@srid}"
+    srs_database = RGeo::CoordSys::SRSDatabase::Proj4Data.new('epsg')
+    shp_factory = RGeo::Geographic.projected_factory(:projection_proj4 => "+proj=lcc +lat_1=41.7 +lat_2=40.43333333333333 +lat_0=39.66666666666666 +lon_0=-82.5 +x_0=600000 +y_0=0 +ellps=GRS80 +datum=NAD83 +to_meter=0.3048006096012192 +no_defs"
+                                                     )
+    RGeo::Shapefile::Reader.open(@shpfile, factory: shp_factory.projection_factory) do |file|
       puts "File contains #{file.num_records} records."
       File.open(@textfile_name, 'w') do |outfile|
         outfile.write(@textfile_field_names.join("\t"))
@@ -18,20 +21,23 @@ class ShapefileConverter
 
           puts "record number #{record.index}:"
           puts "  Attributes: #{record.attributes.inspect}"
-
-          factory3857 = RGeo::Geographic.spherical_factory(srid: 3857)
+          puts "---"
+          puts record.geometry.inspect
+          puts "xxx"
+          #factory3857 = RGeo::Geographic.spherical_factory(srid: 3857)
           
-          proj_record = RGeo::Feature.cast(record.geometry, {:factory => factory3857, :project => true})
+          #proj_record = RGeo::Feature.cast(record.geometry, {:factory => factory3857, :project => true})
 
           write_array = Array.new
           @shapefile_field_names.each do |key|
             write_array.push(record.attributes[key])
           end
           write_array.push(@source_id)
-          puts record.geometry.inspect
-          puts proj_record.inspect  
+       
+          proj_geom = shp_factory.unproject(record.geometry) 
+          puts proj_geom.inspect 
           print "----------------------------------------------------\n\n\n"
-          write_array.push(proj_record.as_text)
+          #write_array.push(proj_record.as_text)
           outfile.write(write_array.join("\t"))
           outfile.write("\n")
         end
@@ -64,5 +70,5 @@ class MPSSCShapefileConverter < ShapefileConverter
   end
 end
 
-sc = CVNPShapefileConverter.new
+sc = MPSSCShapefileConverter.new
 sc.convert_file()
